13342fa116ac3764ffaa615e911ab7be374dcff1
gkarthiks <github.gkarthiks@gmail.com>
2020-07-29 00:29:34 +0530

    chore: re-factored code
    Signed-off-by: gkarthiks <github.gkarthiks@gmail.com>

--- globals/globalVars.go
+++ globals/globalVars.go
@@ -1,13 +1,13 @@
 package globals
 
-import discovery "github.com/gkarthiks/k8s-discovery"
+import (
+	discovery "github.com/gkarthiks/k8s-discovery"
+)
 
 var (
-	VaultIPList   map[string]string
 	K8s           *discovery.K8s
 	Namespace     string
 	HttpTimeout   string
-	LabelSelector string
 )
 
 const (
